29780aa2380c60867646f18a7931936b9ca9c994
CosVigil <cvigil@udesa.edu.ar>
2020-10-13 21:59:04 -0300

    practica8

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//1
+alert("Bienvenidos a mi sitio!!!")
+
+//2
+let = confirm("¿Listo para proceder?")
+if (resultado==false) {
+document.querySelector("h2").innerText="Lamentamos que no quieras continuar tu visita por este sitio increíble."
+} 
+
+if (resultado==true){
+document.querySelector("h3").innerText="¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!"
+}
+
